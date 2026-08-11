90ffbc786b100404e2264a959a4d200fc0972ea8
Davi Silva Buhrer <dsbuhrer@gmail.com>
2026-08-11 19:01:15 -0300

    feat: integrate Google Analytics tracking by adding gtag.js script to index.html and htmlLayout.mjs

--- scripts/seo/htmlLayout.mjs
+++ scripts/seo/htmlLayout.mjs
@@ -57,6 +57,15 @@ export function renderHtmlPage({
     .prose-blog img{max-width:100%;height:auto}
   </style>
   ${ld}
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-X29FRJKBE5"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-X29FRJKBE5');
+  </script>
 </head>
 <body>
   <div id="root">${body}</div>
